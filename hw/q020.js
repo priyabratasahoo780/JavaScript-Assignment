27d556af725068014cb00047a475735d2b444b2d
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2025-10-18 17:14:23 +0530

    f

--- hw/q020.js
+++
@@ -1,8 +0,0 @@
-// Q20. Create a new array that contains only elements at even indices from [10, 20, 30, 40, 50, 60].
-
-let originalArray = [10, 20, 30, 40, 50, 60];
-
-
-let evenIndicesArray = originalArray.filter((element, index) => index % 2 === 0);
-
-console.log(evenIndicesArray); 
