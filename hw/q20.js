0354fa6b53ef30bd46900a7a2a17b623d9f1c4d1
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2025-10-18 17:01:50 +0530

    add

--- hw/q20.js
+++ hw/q20.js
@@ -5,6 +5,4 @@ let originalArray = [10, 20, 30, 40, 50, 60];
 
 let evenIndicesArray = originalArray.filter((element, index) => index % 2 === 0);
 
-console.log(evenIndicesArray);  
-
-u5y5
+console.log(evenIndicesArray); 
